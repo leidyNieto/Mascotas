a2b6c22401584a6c87761b38a067d8c8384b98b8
JeronimoGarp <jeronimo.garcia26502@ucaldas.edu.co>
2023-08-20 17:03:27 -0500

    Se corrigio la linea de mongodb.js para que tuviera bien la base de datos

--- model/db-connection/mongodb.js
+++ model/db-connection/mongodb.js
@@ -5,7 +5,7 @@ const config = require("config");
 
 const mongodbInfo = config.get("db-connections.mongodb");
 
-const connStr = `mongodb+srv://${mongodbInfo.user}:${mongodbInfo.pasword}@${mongodbInfo.host}`
+const connStr = `mongodb+srv://${mongodbInfo.user}:${mongodbInfo.pasword}@${mongodbInfo.host}${mongodbInfo.dbname}`
 
 module.exports= () => {
     mongoose.connect(connStr);
